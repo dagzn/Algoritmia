70ffa3b4dc00cf03def5e519b6d861cbd6c917a0
DanielDumile <daniel_gn98@hotmail.com>
2017-08-30 11:46:39 -0500

    TEST

--- Marbles.cpp
+++ Marbles.cpp
@@ -4,27 +4,6 @@
 //#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
 using namespace std;
 
-
-/*
-int binarySearch(int ini, int fin, int arr[],int buscar){
-	int pos = (fin - ini)/2;
-
-	//cout << "Entre" << arr.size() << endl;
-
-	if(ini == fin)
-		cout << "Que pinche pedo" << endl;
-		return 10;
-	if(buscar == arr[pos])
-		return pos;
-	if(buscar < arr[pos])
-		//cout << "mande1" <<endl;
-		binarySearch(ini,pos,arr,buscar);
-	if(buscar > arr[pos])
-		//cout << "mande2" <<endl;
-		binarySearch(pos+1,fin,arr,buscar);
-	
-}*/
-
 int binarySearch(int left, int right, int arr[], int value) {
       while (left <= right) {
             int middle = (left + right) / 2;
@@ -47,51 +26,63 @@ int checar(int num, int pos, int arr[]){
 
 
 int main(){
-	int N,Q,aux,resul;
+	int N,Q,aux,resul,caso=0;
 	
 	vector<int> query;
 	vector<int> resultados;
 
 	//optimizar_io
+	//optimizar_io
 
 	cin >> N >> Q;
 	
-	int marbles[N];
+	while(N != 0 && Q != 0){
+		caso++;
+		int marbles[N];
 
-	for (int i = 0; i < N; ++i)
-	{
-		cin >>marbles[i];
-		//marbles.push_back(aux);
-	}
-	
-	for (int j = 0; j < Q; ++j)
-	{
-		cin >> aux;
-		query.push_back(aux);
-	}
-
-	sort(marbles,marbles+N);
-
-	
-	for (int k = 0; k < Q; ++k)
-	{	
-		resultados.push_back(binarySearch(0,N-1,marbles,query[k]));
-	}
-	
-	for (int i = 0; i < Q; ++i)
-	{
-		if( resultados[i] > 0){
-			resultados[i] = checar(marbles[resultados[i]],resultados[i],marbles);
+		for (int i = 0; i < N; ++i)
+		{
+			cin >>marbles[i];
+			//marbles.push_back(aux);
 		}
+		
+		for (int j = 0; j < Q; ++j)
+		{
+			cin >> aux;
+			query.push_back(aux);
+		}
+
+		sort(marbles,marbles+N);
+
+		
+		for (int k = 0; k < Q; ++k)
+		{	
+			resultados.push_back(binarySearch(0,N-1,marbles,query[k]));
+		}
+		
+		for (int i = 0; i < Q; ++i)
+		{
+			if( resultados[i] > 0){
+				resultados[i] = checar(marbles[resultados[i]],resultados[i],marbles);
+			}
+		}
+
+		cout << "CASE# " << caso << ":"<< endl;
+
+		for (int i = 0; i < resultados.size(); ++i)
+		{
+			if(resultados[i] < 0)
+				cout << query[i]<<" not found"<< endl;
+			else
+				cout << query[i] <<" found at " << resultados[i] + 1 << endl;
+		}
+
+		query.clear();
+		resultados.clear();
+		cin >> N >> Q;
 	}
 
-	for (int i = 0; i < resultados.size(); ++i)
-	{
-		if(resultados[i] < 0)
-			cout << "El numero no esta"<< endl;
-		else
-			cout <<"Resultado:" << resultados[i]<< endl;
-	}
+	
 	
 	return 1; 
 }

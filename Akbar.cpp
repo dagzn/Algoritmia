59343466bc1c389d2fb24b238d436e3cfd7b3ee5
DanielDumile <daniel_gn98@hotmail.com>
2017-11-23 15:04:42 -0600

    Checar caso de la SPOJ

--- Akbar.cpp
+++ Akbar.cpp
@@ -4,13 +4,16 @@ using namespace std;
 bool undirected = true;
 vector<vector<int>> adj;
 vector<bool> checked;
+vector<int> turnos;
 vector<pair<int,int>> nodeStrength;
 int protegidos=0;
+int turno =0;
 //vector<bool> sourceNode;
 
 
 void initialize(int n){
 	adj.resize(n);
+	turnos.resize(n);
 	//sourceNode.resize(n,false);
 	//padres.resize(n);
 	checked.resize(n,false);
@@ -32,43 +35,55 @@ void addSoldier(int u, int s){
 
 bool bfs(int u,int s){
 	unordered_map <int,int> parent;
+	unordered_map<int,bool> processedTurn;
 	queue<int> q;
 	int ultimo=u;
 	if(checked[u]){
-		//cout << "Trone al entrar con: " << u << endl;
+		cout << "Trone al entrar con: " << u << endl;
 		return false;
 	}
 	q.push(u);
-	parent[u] = -1;
+	
+	processedTurn[u] = true;
 	checked[u]=true;
+	//turnos[u] = turno;
 	protegidos++;
 	int v,cont=0;
 	while(cont < s){
 		v = q.front();
 		q.pop();
 		for (int i = 0; i < adj[v].size(); ++i){
-			if(parent[v] == adj[v][i])
+			cout << "Checo arista de " << v+1 << " hacia " << adj[v][i]+1 << endl;
+			if(processedTurn.count(adj[v][i]) > 0)
 				continue;
 
 			if(!checked[adj[v][i]]){
 				checked[adj[v][i]] = true;
+				//turnos[adj[v][i]] = turno;
+
+				processedTurn[adj[v][i]] = true;
+				
 				protegidos++;
 				q.push(adj[v][i]);
-				parent[adj[v][i]] = v;
+				//parent[adj[v][i]] = v;
 
-				//cout << "Agrego a: " << adj[v][i] << endl;
+				cout << "Agrego a: " << adj[v][i] +1<< endl;
 				//cout << "Agrego padre de: " << adj[v][i] << " es " << v << endl;
 				//cout << "El i es de: " << i << " el tamanio es de: "<< adj[v].size() << "y el v es: " << v << " y el ultimo es: " << ultimo << endl;
 				if(i == adj[v].size() -1 && v == ultimo){
 					ultimo = adj[v][i];
-					//cout << "El ultimo es: " << ultimo << endl;
+					cout << "El ultimo es: " << ultimo +1<< endl;
 					cont++;
 				}
 			}
 			else{
+				
 				//cout << "El padre de: " << v << " es " << parent[v];
-				//cout << "Trono por que topo con: " << adj[v][i]  << endl;
-				return false;
+				cout << "Trono por que topo con: " << adj[v][i] +1 << endl;
+				//cout << "El turno en el que estas es: " << turno << "y el turno del nodo es: " << turnos[adj[v][i]] << endl;
+				if(!processedTurn.count(adj[v][i])){
+					return false;
+				}
 			}
 		}
 		//checked[v] = true;
@@ -96,6 +111,7 @@ int main(){
 		}
 		bool flag = true;
 		for (int i = 0; i < nodeStrength.size(); ++i){
+			turno++;
 			if(!bfs(nodeStrength[i].first,nodeStrength[i].second)){
 				cout << "No\n";
 				flag = false;
@@ -112,6 +128,7 @@ int main(){
 		adj.clear();
 		checked.clear();
 		nodeStrength.clear();
+		turnos.clear();
 		protegidos=0;
 		//sourceNode.clear();
 	}

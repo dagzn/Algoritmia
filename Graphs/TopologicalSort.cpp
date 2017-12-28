622392483450ea26502d2a6898523bc78e1c72fe
DanielDumile <daniel_gn98@hotmail.com>
2017-12-28 00:21:48 -0600

    Update TopologicalSort.cpp

--- Graphs/TopologicalSort.cpp
+++ Graphs/TopologicalSort.cpp
@@ -1,57 +1,63 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool undirected;
-vector<vector<int>> adj;
-vector<bool> checked;
-stack<int> s;
+class Graph
+{
+public:
+	bool undirected;
+	vector<vector<int>> adj;
+	vector<int> checked;
+	stack<int> s;
 
-
-void initialize(int n, bool u){
-	adj.resize(n);
-	checked.resize(n,false);
-	undirected = u;
-}
-
-void addEdge(int u, int v){
-	adj[u].push_back(v);
-	if(undirected)
-		adj[v].push_back(u);
-}
-
-void dfsvisit(int u){
-	cout << "Visiting: " << u+1 << endl;
-	checked[u] = true;
-	for (int v = 0; v < adj[u].size(); ++v)
-	{
-		if(!checked[adj[u][v]])
-			dfsvisit(adj[u][v]);		
+	Graph(bool undirected,int n){
+		this->undirected = undirected;
+		adj.resize(n);
+		checked.resize(n);
 	}
-	s.push(u);
-}
 
-void dfs(){
-	dfsvisit(rand()%8);
-	for (int i = 0; i < adj.size(); ++i){
-		if(!checked[i])
-			dfsvisit(i);
+	void addEdge(int u, int v){
+		adj[u].push_back(v);
+		if(undirected)
+			adj[v].push_back(u);
 	}
-}
 
+	void dfs(int u){
+		cout << "Visiting: " << u+1 << endl;
+		checked[u] = true;
+		for (int v = 0; v < adj[u].size(); ++v)
+		{
+			if(!checked[adj[u][v]])
+				dfs(adj[u][v]);		
+		}
+		s.push(u);
+	}
 
+	stack<int> topological(){
+		//dfsvisit(rand()%8);
+		for (int i = 0; i < adj.size(); ++i){
+			if(!checked[i])
+				dfs(i);
+		}
+		return s;
+	}
+	
+};
 
 int main(){
-	initialize(8,false);
-	addEdge(0,2);
-	addEdge(1,2);
-	addEdge(1,3);
-	addEdge(2,4);
-	addEdge(3,5);
-	addEdge(4,5);
-	addEdge(5,6);
-	addEdge(4,7);
-	dfs();
 	char aux;
+
+	Graph *g = new Graph(false,8);
+	g->addEdge(0,2);
+	g->addEdge(1,2);
+	g->addEdge(1,3);
+	g->addEdge(2,4);
+	g->addEdge(3,5);
+	g->addEdge(4,5);
+	g->addEdge(5,6);
+	g->addEdge(4,7);
+
+	stack<int> s = g->topological();
+	
 	while(!s.empty()){
 		aux = (char) (s.top()+65);
 		cout << aux << endl;

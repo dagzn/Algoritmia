3dc8afc612d3d2de08ef63e0f1064e9d3465f610
DanielDumile <daniel_gn98@hotmail.com>
2017-12-28 00:39:56 -0600

    Change checked vector for an unordered set

--- Graphs/StronglyConnectedComponents.cpp
+++ Graphs/StronglyConnectedComponents.cpp
@@ -1,96 +1,107 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool undirected;
-vector<vector<int>> adj;
-vector<vector<int>> otro;
-vector<vector<int>> cc;
-vector<int> components;
-vector<bool> checked;
-stack<int> s;
 
 
-void initialize(int n, bool u){
-	adj.resize(n);
-	checked.resize(n,false);
-	otro.resize(n);
-	undirected = u;
-}
 
-void addEdge(int u, int v){
-	adj[u].push_back(v);
-	otro[v].push_back(u);
-	if(undirected)
-		adj[v].push_back(u);
-}
+class Graph
+{
+public:
+	bool undirected;
+	vector<vector<int>> adj;
+	vector<vector<int>> otro;
+	vector<vector<int>> cc;
+	vector<int> components;
+	vector<int> checked;
+	stack<int> s;
 
-void dfsvisit(int u, bool num){
-	checked[u] = true;
-	for (int v = 0; v < adj[u].size(); ++v)
-	{
-		if(!checked[adj[u][v]])
-			dfsvisit(adj[u][v],num);		
+	Graph(bool undirected, int n){
+		adj.resize(n);
+		checked.resize(n,false);
+		otro.resize(n);
+		this->undirected = undirected;
 	}
-	if(num)
-		s.push(u);
-	else
-		components.push_back(u);
-}
 
-void dfsFirst(){
-	for (int i = 0; i < adj.size(); ++i){
-		if(!checked[i])
-			dfsvisit(i,true);
+	void addEdge(int u, int v){
+		adj[u].push_back(v);
+		otro[v].push_back(u);
+		if(undirected)
+			adj[v].push_back(u);
 	}
-}
 
-void dfsSecond(){
-	int aux;
-	for (int i = 0; i < adj.size(); ++i)
-	{
-		aux = s.top();
-		s.pop();
-		if(!checked[aux]){
-			//components.push_back(aux);
-			dfsvisit(aux,false);
-			cc.push_back(components);
-			components.clear();
+	vector<vector<int>> reverseGraph(){
+		for (int i = 0; i < checked.size(); ++i){
+			checked[i] = false;
+		}
+		return otro;
+	}
+
+	void dfsvisit(int u, bool num){
+		checked[u] = true;
+		for (int v = 0; v < adj[u].size(); ++v)
+		{
+			if(!checked[adj[u][v]])
+				dfsvisit(adj[u][v],num);		
+		}
+		if(num)
+			s.push(u);
+		else
+			components.push_back(u);
+	}
+
+	void dfsFirst(){
+		for (int i = 0; i < adj.size(); ++i){
+			if(!checked[i])
+				dfsvisit(i,true);
+		}
+		dfsSecond();
+	}
+
+	void dfsSecond(){
+		int aux;
+		adj = reverseGraph();
+		for (int i = 0; i < adj.size(); ++i)
+		{
+			aux = s.top();
+			s.pop();
+			if(!checked[aux]){
+				//components.push_back(aux);
+				dfsvisit(aux,false);
+				cc.push_back(components);
+				components.clear();
+			}
 		}
 	}
-}
-
-vector<vector<int>> reverseGraph(){
-	for (int i = 0; i < checked.size(); ++i){
-		checked[i] = false;
+public:
+	void printCC(){
+		dfsFirst();
+		char car;
+		for (int i = 0; i < cc.size(); ++i){
+			cout << "Strongly CC number " << i+1 << endl;
+			for (int j = 0; j < cc[i].size(); ++j){
+				car = (char) (cc[i][j]+65);
+				cout << car << endl;
+			}
+		}
 	}
-	return otro;
-}
+	
+};
 
 int main(){
-	initialize(11,false);
-	addEdge(0,1);
-	addEdge(1,2);
-	addEdge(2,0);
-	addEdge(1,3);
-	addEdge(3,4);
-	addEdge(4,5);
-	addEdge(5,3);
-	addEdge(6,5);
-	addEdge(6,7);
-	addEdge(7,8);
-	addEdge(8,9);
-	addEdge(9,6);
-	addEdge(9,10);
-	dfsFirst();
-	adj = reverseGraph();
-	dfsSecond();
-	char car;
-	for (int i = 0; i < cc.size(); ++i){
-		cout << "Strongly CC number " << i+1 << endl;
-		for (int j = 0; j < cc[i].size(); ++j){
-			car = (char) (cc[i][j]+65);
-			cout << car << endl;
-		}
-	}
+	Graph *g = new Graph(false,11);
+	g->addEdge(0,1);
+	g->addEdge(1,2);
+	g->addEdge(2,0);
+	g->addEdge(1,3);
+	g->addEdge(3,4);
+	g->addEdge(4,5);
+	g->addEdge(5,3);
+	g->addEdge(6,5);
+	g->addEdge(6,7);
+	g->addEdge(7,8);
+	g->addEdge(8,9);
+	g->addEdge(9,6);
+	g->addEdge(9,10);
+	g->printCC();
 	return 0;
 }

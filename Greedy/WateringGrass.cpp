9ed187a1baf158c4ce32e242d891c7ce45428a9e
DanielDumile <daniel_gn98@hotmail.com>
2018-02-21 23:08:09 -0600

    Update WateringGrass.cpp

--- Greedy/WateringGrass.cpp
+++ Greedy/WateringGrass.cpp
@@ -1,62 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef pair<int,int> pii;
-typedef pair<float,float> pff;
+typedef pair<double,double> pff;
 
-
-bool checkRange(pff r, pff o){
-	if(r.first <= o.first){
-		if(r.second <= o.first)
-			return false;
-		else
-			return true;
-	}
-	return false;
-}
-
-vector<pff> minCoverage(vector<pff> ranges, pff origin){
+void minCoverage(priority_queue<pff> pq, pff origin){
 	vector<pff> res;
+	
+	bool covered = false;
 	while(true){
-		int reach=origin.first;
 		pff max = pff(0,0);
-		for (int i = 0; i < ranges.size(); ++i){
-			if(checkRange(ranges[i],origin)){
-				if(ranges[i].second > reach)
-					max = ranges[i];
+		double reach=origin.first;
+		
+		while (!pq.empty()){
+			pff aux = pq.top();
+			
+			if((-aux.first) <= origin.first){			
+				if(aux.second > origin.first){
+					if(aux.second > reach){
+						max = pff(-aux.first,aux.second);
+						reach = aux.second;
+					}
+				}
+				pq.pop();
 			}
+			else
+				break;
 		}
+		
+
 		if(max.first == 0 && max.second == 0)
 			break;
 		res.push_back(max);
-		if(max.second >= origin.second)
+		if(max.second >= origin.second){
+			covered = true;
 			break;
+		}
 		else
 			origin.first = max.second;
 	}
-	return res;
+	if(covered){
+		if(res.size() != 0)
+			cout << res.size() << "\n";
+	}
+	else
+		cout << "-1\n";
+	
 }
 
 int main(){
-	float n,l,w;
+	double n,l,w;
+	ios_base::sync_with_stdio(0);
 	while(cin >> n >> l >> w){
-		float p,r;
-		vector<pff> pos;
+		double p,r;
+		//vector<pff> pos;
+		priority_queue<pff> pos;
 		for (int i = 0; i < n; ++i){
 			cin >> p >> r;
-			if(r >= w/2){
-				float root = pow(r,2) - pow((w/2),2);
-				//cout << "La raiz es: " << root << endl;
-				float a = p - sqrt(root);
-				float b = p + sqrt(root);
-				pos.push_back(pff(a,b));
-				//cout <<"Rango: " << a << " " << b << "\n";
+			if((r*2) > w){
+				double root;
+				double a,b;
+				root = (sqrt(4*pow(r,2) -(pow(w,2))))/2;
+				a = p - root;
+				b = p + root;
+				//cout << "Rangos son: " << a << " " << b << endl;
+				pos.push(pff(-a,b));
 			}
+			//checkSprinkler(p,r,w,pos);
 		}
-		vector<pff> res = minCoverage(pos,pff(0,l));
-		if(res.size())
-			cout << res.size() << "\n";
-		else
-			cout << "-1\n";
+		minCoverage(pos,pff(0,l));
 	}
 	return 0;
 }

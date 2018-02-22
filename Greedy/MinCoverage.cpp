964c33b88d8b4b905c9b222e1450a98a73ca3895
DanielDumile <daniel_gn98@hotmail.com>
2018-02-21 23:08:51 -0600

    Update MinCoverage.cpp

--- Greedy/MinCoverage.cpp
+++ Greedy/MinCoverage.cpp
@@ -3,55 +3,73 @@ using namespace std;
 typedef pair<int,int> pii;
 
 
-vector<pii> minCoverage(priority_queue<pii> ranges, pii origin){
+void minCoverage(priority_queue<pii> pq, pii origin){
 	vector<pii> res;
+	//priority_queue<pii> pq;
+	bool covered = false;
 	while(true){
-		int reach=origin.first;
 		pii max = pii(0,0);
-		for (int i = 0; i < ranges.size(); ++i){
-			pii aux = ranges.top();
-			if(-aux.first <= origin.first){
+		int reach=origin.first;
+		
+		while (!pq.empty()){
+			pii aux = pq.top();
+			//cout << -aux.first<<" " << aux.second << endl;
+			if((-aux.first) <= origin.first){			
 				if(aux.second > origin.first){
-					if(aux.second > reach)
+					if(aux.second > reach){
 						max = pii(-aux.first,aux.second);
+						reach = aux.second;
+					}
 				}
-				ranges.pop();
+				pq.pop();
 			}
 			else
 				break;
 		}
+		
 		if(max.first == 0 && max.second == 0)
 			break;
 		res.push_back(max);
-		if(max.second >= origin.second)
+		if(max.second >= origin.second){
+			covered = true;
 			break;
+		}
 		else
 			origin.first = max.second;
 	}
-	return res;
+	if(covered){
+		cout << res.size() << endl;
+		for (int i = 0; i < res.size(); ++i){
+			cout << res[i].first << " " << res[i].second << "\n"; 
+		}
+	}
+	else{
+		cout << "0\n";
+	}
+	
 }
 
 int main(){
 	int T,M;
+	ios_base::sync_with_stdio(0);
 	cin >> T;
 	for (int i = 0; i < T; ++i)
 	{
 		int a,b;
-		vector<pii> ranges;
-		priority_queue<pii> pq;
+		priority_queue<pii> ranges;
+		//vector<pii> rangos;
 		cin >> M;
 		while(true){
 			cin >> a >> b;
 			if(!a && !b)
 				break;
-			//ranges.push_back(pii(a,b));
-			pq.push(pii(-a,b));
+			ranges.push(pii(-a,b));
+			//rangos.push_back(pii(a,b));
 		}
-		vector<pii> res = minCoverage(pq,pii(0,M));
-		cout << res.size() << endl;
-		for (int i = 0; i < res.size(); ++i){
-			cout << res[i].first << " " << res[i].second << "\n"; 
-		}
+		
+		minCoverage(ranges,pii(0,M));
+		if(i != T-1)
+			cout << "\n";
 	}
 	return 0;
 }

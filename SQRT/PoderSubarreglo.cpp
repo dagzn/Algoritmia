a0c79bc64977cd9f1d2f591e21766252253a38ab
DanielDumile <daniel_gn98@hotmail.com>
2018-03-20 21:06:04 -0600

    Update PoderSubarreglo.cpp

--- SQRT/PoderSubarreglo.cpp
+++ SQRT/PoderSubarreglo.cpp
@@ -1,5 +1,156 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int block;
+struct Query
+{
+    int L, R,id;
+};
+typedef Query* queer;
+ 
+bool compare(queer x, queer y)
+{
+    if (x->L/block != y->L/block)
+        return x->L/block < y->L/block;
+ 
+    return x->R < y->R;
+}
+ 
+
+void queryResults(vector<int> &a, int n, vector<queer> &q, int m)
+{
+    
+    block = ceil(sqrt(n));
+    vector<int> resultados(m);
+    int res = 0;
+    int num = 0;
+    sort(q.begin(), q.end(), compare);
+    
+
+    unordered_map<int,int> numbers;
+    
+    int l = q[0]->L;
+    int r = q[0]->R;
+    //cout << "Hacemos a " << l << " " << r << endl;
+    for (int i = l; i <= r; ++i){
+        int num = a[i];
+        if(numbers.count(num)){
+            numbers[num]++;
+        }
+        else{
+            numbers[num]=1;
+        }
+        //res+= (2*numbers[num]+1);
+    }
+    
+    for (auto it = numbers.begin(); it != numbers.end(); ++it ){
+		res+= pow(it->second,2) * it->first;
+	}
+	
+    resultados[q[0]->id] = res; 
+
+    int currL = l, currR = r;
+    for (int i=1; i<m; i++)
+    {
+    	//res = 0;
+        int L = q[i]->L, R = q[i]->R;
+        //cout << "Hacemos a " << L << " " << R << endl;
+        while (currL < L)
+        {
+            num = a[currL];
+
+            numbers[num]--;       
+            //cout << "Quitamos a " << num << " y quitamos a res un " << (2*numbers[num]+1) << endl;
+            res-= (2*numbers[num]+1);
+            if(numbers[num] == 0){
+                numbers.erase(num);
+            }
+            currL++;
+        }
+        while (currL > L)
+        {
+            num = a[currL-1];
+            //cout << "Sumamos a " << num << " y sumamos a res un " << (2*numbers[num]+1) << endl;
+            res+= (2*numbers[num]+1);
+            if(numbers.count(num))
+                numbers[num]++;
+            else
+                numbers[num]=1;
+            
+            currL--;
+        }
+        while (currR < R)
+        {
+            num = a[currR+1];
+            //cout << "Sumamos a " << num << " y sumamos a res un " << (2*numbers[num]+1) << endl;
+            res+= (2*numbers[num]+1);        
+            if(numbers.count(num)){
+                numbers[num]++;
+            }
+            else{
+                numbers[num]=1;
+            }
+            
+            currR++;
+        }
+ 
+        while (currR > R)
+        {
+            num = a[currR];
+            
+            numbers[num]--;
+            res-= (2*numbers[num]+1);
+            //cout << "Quitamos a " << num << " y quitamos a res un " << ((2*numbers[num+1])+1) << endl;    
+            if(numbers[num] == 0){
+                numbers.erase(num);
+            }
+            currR--;
+        }
+        /*
+    	for (auto it = numbers.begin(); it != numbers.end(); ++it ){
+			res+= pow(it->second,2) * it->first;
+		}*/
+		//cout << "El resultado fue " << res << endl;
+        resultados[q[i]->id]=res; 
+    }
+    for (int i = 0; i < resultados.size(); ++i)
+    {
+        cout << resultados[i] << "\n";
+    }
+}
+/*
+int main()
+{
+    int n,m,x,y;
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    cin >> n >> m;
+    
+    vector<int> a(n);
+    vector<queer> q(m);
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> x;
+        a[i]=x;
+    }
+    
+    //cin >> m;
+    
+    
+    
+    for (int i = 0; i < m; ++i)
+    {
+        cin >> x >> y;
+        queer aux = new Query();
+        aux->L = x-1;
+        aux->R = y-1;
+        aux->id=i;
+        q[i]=aux;    
+    }
+    queryResults(a, n, q, m);
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
 typedef pair<int,int> pii;
 int main(){
 	int n,q,stop;
@@ -136,4 +287,4 @@ int main(){
 	
 	
 	return 0;
-}
+}*/

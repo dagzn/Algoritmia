c29aa4ca412c0a0844bd6194e54b1bcee088a7f3
DanielDumile <daniel_gn98@hotmail.com>
2019-03-07 12:18:22 -0600

    Create Plantilla.cpp

---
+++ Reference/Plantilla.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define forn(i,n) for(int i =0; i < n; i++)
+#define pb push_back
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef long long int lli;
+const lli inf = 4000000000000LL; 

b2c6fa4de1b9a588dbc28d2a89bc94569a21f718
DanielDumile <daniel_gn98@hotmail.com>
2019-03-07 21:58:22 -0600

    Update Plantilla.cpp

--- Reference/Plantilla.cpp
+++ Reference/Plantilla.cpp
@@ -6,8 +6,9 @@ using namespace std;
 #define all(x) x.begin(),x.end()
 #define forn(i,n) for(int i =0; i < n; i++)
 #define pb push_back
+#define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long long int lli;
-const lli inf = 4000000000000LL; 
+const lli inf = 4000000000000LL;

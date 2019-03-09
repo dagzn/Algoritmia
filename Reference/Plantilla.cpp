206d3fc7acacf1d786e2eb926c047fd1e038e5f0
DanielDumile <daniel_gn98@hotmail.com>
2019-03-08 19:03:31 -0600

    Final

--- Reference/Plantilla.cpp
+++ Reference/Plantilla.cpp
@@ -5,10 +5,15 @@ using namespace std;
 #define se second
 #define all(x) x.begin(),x.end()
 #define forn(i,n) for(int i =0; i < n; i++)
+#define forr(i,a,n) for(int i = a; i < n; i++)
+#define mem(a,v) memset(a , v, sizeof a)
 #define pb push_back
+#define mp make_pair
 #define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long long int lli;
-const lli inf = 4000000000000LL;
+#define MOD 1000000007
+#define INF 1<<30
+//const lli inf = 4000000000000LL;

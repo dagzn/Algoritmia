de592ba0183759d3d7f3a9f7dc6ba9e9e4faf430
DanielDumile <daniel_gn98@hotmail.com>
2019-03-13 17:32:10 -0600

    Update Plantilla.cpp

--- Reference/Plantilla.cpp
+++ Reference/Plantilla.cpp
@@ -7,13 +7,16 @@ using namespace std;
 #define forn(i,n) for(int i =0; i < n; i++)
 #define forr(i,a,n) for(int i = a; i < n; i++)
 #define mem(a,v) memset(a , v, sizeof a)
+#define prec(n,a) fixed << setprecision(n) << a
 #define pb push_back
 #define mp make_pair
 #define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
+#define MOD(n,k) ( ( ((n) % (k)) + (k) ) % (k))
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef long long int lli;
+typedef long double ld;
 #define MOD 1000000007
 #define INF 1<<30
 //const lli inf = 4000000000000LL;

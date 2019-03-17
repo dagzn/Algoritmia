448fc2bd346152ff7cffd06893479ff65284a256
DanielDumile <daniel_gn98@hotmail.com>
2019-03-17 00:49:17 -0600

    Update Plantilla.cpp

--- Reference/Plantilla.cpp
+++ Reference/Plantilla.cpp
@@ -5,18 +5,18 @@ using namespace std;
 #define se second
 #define all(x) x.begin(),x.end()
 #define forn(i,n) for(int i =0; i < n; i++)
-#define forr(i,a,n) for(int i = a; i < n; i++)
+#define forr(i,a,n) for(int i = a; i <= n; i++)
 #define mem(a,v) memset(a , v, sizeof a)
 #define prec(n,a) fixed << setprecision(n) << a
 #define pb push_back
 #define mp make_pair
 #define optimizar_io ios_base::sync_with_stdio(0);cin.tie(0);
-#define MOD(n,k) ( ( ((n) % (k)) + (k) ) % (k))
+#define GMOD(n,k) ( ( ((n) % (k)) + (k) ) % (k))
+typedef long long int lli;
+typedef long double ld;
 typedef pair<int,int> pii;
 typedef vector<int> vi;
 typedef vector<vi> vvi;
-typedef long long int lli;
-typedef long double ld;
 #define MOD 1000000007
 #define INF 1<<30
 //const lli inf = 4000000000000LL;
